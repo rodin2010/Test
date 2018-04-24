bd51464ef3f6e5f6dd24cbfa95ed7cc3dab7a4be
rodin2010 <haphld@163.com>
2018-04-24 22:39:51 +0800

    OKkjk

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,6 +3,8 @@ public class Test {
 
 	  public static void main(String[] args) {
 		         System.out.println(new Test().test());
+		         
+		         System.out.println("123123");
 		       }
 		   
 		       static int test() {
