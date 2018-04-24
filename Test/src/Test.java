f15183440e3c9670a9841b694f26f154bcac496b
rodin2010 <haphld@163.com>
2018-04-24 22:38:41 +0800

    ok

---
+++ Test/src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+
+	  public static void main(String[] args) {
+		         System.out.println(new Test().test());
+		       }
+		   
+		       static int test() {
+		           int x = 1;
+		         try {
+	           return x;
+		         } finally {
+		              ++x;
+		        }
+		     }
+
+
+}
