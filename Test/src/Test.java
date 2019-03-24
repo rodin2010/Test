e23938a8e6f63185d1f219d5e4b7ed5cdb5ee8b7
luodan <haphld@163.com>
2019-03-24 11:08:57 +0800

    22

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	  public static void main(String[] args) {
 		         System.out.println(new Test().test());
 		         
-		         System.out.println("123123sdafsdff234");
+		         System.out.println("123123sdafsdff234 from barnch2");
 		       }
 		   
 		       static int test() {
