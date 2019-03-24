fa42c6e9fd374885710eb1978730d5a2d3834a8c
luodan <haphld@163.com>
2019-03-24 10:54:14 +0800

    11

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	  public static void main(String[] args) {
 		         System.out.println(new Test().test());
 		         
-		         System.out.println("123123sdafsdff23411");
+		         System.out.println("hebing zhugan ");
 		       }
 		   
 		       static int test() {
